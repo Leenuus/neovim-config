73279508480ba142102af0d5bcaf1b06176977c1
Leenuus <linusrach24@gmail.com>
2023-12-11 19:27:16 +0800

    Add formatter support

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -7,26 +7,31 @@ local filetype_formatter = {
 	fish = { "fish_indent" },
 }
 
+function create_command(conform)
+	vim.api.nvim_create_user_command("Format", function(args)
+		local range = nil
+		if args.count ~= -1 then
+			local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+			range = {
+				start = { args.line1, 0 },
+				["end"] = { args.line2, end_line:len() },
+			}
+		end
+		conform.format({ async = true, lsp_fallback = true, range = range })
+	end, { range = true })
+end
+
 return {
-	'stevearc/conform.nvim',
+	"stevearc/conform.nvim",
 	opts = {},
 	config = function()
 		local conform = require("conform")
 		conform.setup({
-			formatters_by_ft = filetype_formatter
+			formatters_by_ft = filetype_formatter,
+			format_on_save = { timeout_ms = 500, lsp_fallback = true },
 		})
-		vim.api.nvim_create_user_command("Format", function(args)
-			local range = nil
-			if args.count ~= -1 then
-				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-				range = {
-					start = { args.line1, 0 },
-					["end"] = { args.line2, end_line:len() },
-				}
-			end
-			require("conform").format({ async = true, lsp_fallback = true, range = range })
-		end, { range = true })
-	end
+		create_command(conform)
+	end,
 }
 
 -- {
